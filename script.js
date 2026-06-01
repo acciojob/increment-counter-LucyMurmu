51d8bcd1d91bdc2e8b9c7ace0f1607c8c9dcd025
LucyMurmu <btech10543.22@bitmesra.ac.in>
2026-06-02 00:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let count=0;
 document.getElementById("increment-counter").addEventListener("click",function(){
 	alert(count);
 	count++;
-	document.getElementById("counter").textContent="count";
+	document.getElementById("counter");
 });
